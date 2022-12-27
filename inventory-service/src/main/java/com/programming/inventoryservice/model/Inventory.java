869aae4135bd9e87124d48241e801213a846c619
Sahak-S <sahak.sa@mail.ru>
2022-12-28 00:43:45 +0400

    microservice

---
+++ inventory-service/src/main/java/com/programming/inventoryservice/model/Inventory.java
@@ -0,0 +1,22 @@
+package com.programming.inventoryservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "t_inventory")
+@Entity
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+}
