bcd911d1b8d5c098da7577560cac1c81a4f93299
Sahak-S <sahak.sa@mail.ru>
2022-12-28 22:08:54 +0400

    add

--- order-service/src/main/java/com/programming/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/programming/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.programming.orderservice.service;
 
+import com.programming.orderservice.dto.InventoryResponse;
 import com.programming.orderservice.dto.OrderLineItemsDto;
 import com.programming.orderservice.dto.OrderRequest;
 import com.programming.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.programming.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,8 +21,9 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -28,7 +32,26 @@ public class OrderService {
                 .map(this::mapToDto)
                 .toList();
         order.setOrderLineItemsList(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+        //Զանգահարեք գույքագրման ծառայություն և պատվիրեք, եթե ապրանքը կա
+        //ակցիա stock
+        InventoryResponse[] inventoryResponsArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsArray).allMatch(InventoryResponse::isInStock);
+        if (allProductsInStock){
+            orderRepository.save(order);
+        }else {
+            throw new IllegalArgumentException("ապրանքը պահեստում չէ խնդրում ենք փորցել կրկին");
+        }
+
 
     }
 
